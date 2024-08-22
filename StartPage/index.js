7d959998ec80b51939d624698e153a7e640caa7d
Trananhduc19092005 <duc19092005f@gmail.com>
2024-08-23 03:03:34 +0700

    VanNgan Food

---
+++ StartPage/index.js
@@ -0,0 +1,6 @@
+const element = document.querySelector('#icon');
+const menu = document.querySelector('.menu');
+button.addEventListener('click', () => {
+  menu.classList.toggle('active');   
+
+});
